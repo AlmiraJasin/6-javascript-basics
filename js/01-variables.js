fce2214c2e768deaaa3333074c81acb76af8a63c
Almira Jasinskyte <jasinskyte.almira@gmail.com>
2022-04-06 00:48:52 +0300

    variables

---
+++ js/01-variables.js
@@ -0,0 +1,34 @@
+/*
+Kintamieji
+
+Inicijavimo budas:
+const -
+let -
+var-
+
+Duomenu tipas: number, string (tekstas), boolean, array (masyvas, sarasas, matrica)
+*/
+const luckyNumber = 7;
+console.log(luckyNumber);
+
+let pinigine = 0;
+console.log(pinigine);
+
+pinigine = 5;
+console.log(pinigine);
+
+pinigine = 10;
+console.log(pinigine);
+
+const vardas = 'Almira';
+console.log(vardas);
+
+const pasisveikinimas = 'Laba diena, ' + vardas + '.';
+console.log(pasisveikinimas);
+
+const arVedes = true;
+console.log('Ar vedes?', arVedes);
+
+const pazymiai = [10, 2, 8, 4, 6];
+console.log('Pazymiai ', pazymiai);
+
